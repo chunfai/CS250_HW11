b4bbbb86bee35285d397eb044ba75da933e2850e
chunfai <joeli0419@gmail.com>
2017-11-07 04:31:34 -0600

    added comment

--- app/src/main/java/com/example/joeli/cs250_hw11/MainActivity.java
+++ app/src/main/java/com/example/joeli/cs250_hw11/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //ABCDERF
+        //ABCDERFG
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
